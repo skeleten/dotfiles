31f3ca036d5ee95cca0a5b12a0e9d8c6d5e04dbd
Jan Pelle Thomson <pelle.thomson@yahoo.de>
2017-12-29 03:36:37 +0100

    Update mail cfg

--- emacs/.emacs.d/config/40-mail.el
+++ emacs/.emacs.d/config/40-mail.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path
 	     "/usr/share/emacs/site-lisp/mu4e")
 (require 'mu4e)
+;; Not sure what :match-func does or why it is relevant :(
 (setq mu4e-contexts
       `( ,(make-mu4e-context
 	   :name "skeleten"
@@ -12,6 +13,8 @@
 	   :match-func (lambda (msg)
 			 (when msg
 			   (string-prefix-p "/VKM" (mu4e-message-field msg :maildir)))))))
+;; Bookmarks for mu4e; They go to searches
+;; b <key> to jump to them
 (setq mu4e-bookmarks
       `( ,(make-mu4e-bookmark
 	   :name  "Unread messages"
@@ -24,11 +27,11 @@
 	 ,(make-mu4e-bookmark
 	   :name "Last 7 days"
 	   :query "date:7d..now AND NOT flag:trashed"
-	   :key ?w)
-	  ))
+	   :key ?w)))
 (setq user-full-name "Jan Pelle Thomson"
-      user-mail-address "me@skeleten.me"
-      smtpmail-default-smtp-server "mail.skeleten.me"
+      user-mail-address "me@skeleten.me")
+
+(setq smtpmail-default-smtp-server "mail.skeleten.me"
       smtpmail-smtp-server "mail.skeleten.me"
       smtpmail-smtp-service 587
       smtpmail-smtp-user "me@skeleten.me"
