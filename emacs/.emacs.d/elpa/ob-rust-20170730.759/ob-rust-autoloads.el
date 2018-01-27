1e649bbcebedc51a9167e0313ed12b0962707f68
Jan Pelle Thomson <pelle.thomson@yahoo.de>
2018-01-27 12:37:03 +0100

    Update emacs cfg

---
+++ emacs/.emacs.d/elpa/ob-rust-20170730.759/ob-rust-autoloads.el
@@ -0,0 +1,16 @@
+;;; ob-rust-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ob-rust-pkg.el" "ob-rust.el" "test-ob-rust.el")
+;;;;;;  (23147 56615 269962 527000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ob-rust-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/ob-rust-20170730.759/ob-rust-pkg.el
@@ -0,0 +1,5 @@
+(define-package "ob-rust" "20170730.759" "Org-babel functions for Rust" 'nil :url "https://github.com/micanzhang/ob-rust" :keywords
+  '("rust" "languages" "org" "babel"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
