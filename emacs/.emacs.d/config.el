b885a201207f1fbe2f960f0f909b8be65dcf9ef0
Jan Pelle Thomson <me@skeleten.me>
2018-09-12 09:21:01 +0000

    err

--- emacs/.emacs.d/config.el
+++ emacs/.emacs.d/config.el
@@ -538,22 +538,23 @@ point reaches the beginning or end of the buffer, stop there."
 (add-hook 'markdown-mode-hook 'display-line-numbers-mode)
 
 (add-hook 'org-mode-hook
-          'smartparens-mode)
+	  'smartparens-mode)
 (add-hook 'org-mode-hook
-          'display-line-numbers-mode)
+	  'display-line-numbers-mode)
 (add-hook 'org-mode-hook
-          'org-bullets-mode)
+	  'org-bullets-mode)
 
 (defun skeleten/org/get-org-files ()
   (mapcar (lambda (f)
-            (concat
-             (file-name-as-directory skeleten/org-files-base-dir) f))
-          (directory-files skeleten/org-files-base-dir nil "\\.org$")))
+	    (concat
+	     (file-name-as-directory skeleten/org-files-base-dir) f))
+	  (directory-files skeleten/org-files-base-dir nil "\\.org$")))
 
 (setq org-default-notes-file "~/org/Main.org"
       org-agenda-files (skeleten/org/get-org-files)
       org-log-done 'time)
-(setq org-src-fontify-natively t)
+(setq org-src-fontify-natively t
+      org-return-follows-link t)
 
 (add-hook 'prog-mode-hook 'company-mode)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
